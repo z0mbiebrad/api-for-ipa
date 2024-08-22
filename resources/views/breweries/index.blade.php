d87aa54660c71734344d17fe9f29efd448dd51b5
z0mbiebrad <bradleyt.evans@gmail.com>
2024-08-22 16:58:33 -0400

    trimmed link

--- resources/views/breweries/index.blade.php
+++ resources/views/breweries/index.blade.php
@@ -7,28 +7,35 @@
                         $address = $brewery->street . ', ' . $brewery->city . ', ' . $brewery->state;
                         $encodedAddress = str_replace(' ', '+', $address);
                     @endphp
-                    <div class="my-4 overflow-hidden bg-white shadow-md shadow-[#023E7D] dark:bg-[#002855] sm:rounded-lg">
+                    <div
+                        class="my-4 overflow-hidden bg-white shadow-md shadow-[#023E7D] dark:bg-[#002855] sm:rounded-lg">
                         <div class="p-6 text-gray-900 dark:text-white text-md sm:text-lg">
                             <p class="">{{ $brewery->name }}</p>
                             <p>
-                                <i class="fa-solid fa-phone pr-1"></i>
-                                <a class="underline" href="tel:{{ $brewery->phone }}">
+                                <a class="" href="tel:{{ $brewery->phone }}">
+                                    <i class="fa-solid fa-phone pr-1"></i>
                                     {{ $brewery->phone }}
                                 </a>
                             </p>
                             @if ($brewery->website_url)
-                                <p class="underline">
-                                    <i class="fa-solid fa-globe pr-1"></i>
-                                    <a href="{{ $brewery->website_url }}">
-                                        {{ $brewery->website_url }}
+                                @php
+                                    $url = $brewery->website_url;
+                                    $prefixes = ['https://www.', 'http://www.', 'www.'];
+                                    $trimmedUrl = str_replace($prefixes, '', $url);
+                                @endphp
+                                <p class="">
+                                    <a href="{{ $brewery->website_url }}" class="-pl-1">
+                                        <i class="fa-solid fa-globe pr-1"></i>
+                                        {{ ucwords($trimmedUrl) }}
                                     </a>
                                 </p>
                             @endif
                             @if ($address)
                                 <p>
-                                    <i class="fa-solid fa-map-pin pr-1"></i>
                                     <a href="http://maps.google.com/maps?q={{ $encodedAddress }}"
-                                        target="_blank">{{ $address }}
+                                        target="_blank">
+                                        <i class="fa-solid fa-map-marker-alt pr-1"></i>
+                                        {{ $address }}
                                     </a>
                                 </p>
                             @endif
